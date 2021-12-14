d60bf89d81053d84635e060a30905f6ee3d2ad8d
stefcos24 <stefan22bl@gmail.com>
2021-12-14 23:00:08 +0100

    Created Product Model, Repo. Started with Controllers and Views.

--- BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -1,4 +1,5 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,10 +17,12 @@ namespace BulkyBook.DataAccess.Repository
             _db = db;
             Category = new CategoryRepository(_db);
             CoverType = new CoverTypeRepository(_db);
+            Product = new ProductRepository(_db);
         }
         public ICategoryRepository Category { get; private set; }
 
         public ICoverTypeRepository CoverType { get; private set; }
+        public IProductRepository Product { get; private set; }
 
         public void Save()
         {
---
+++ BulkyBook/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        //INDEX - GET
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objProductList = _unitOfWork.Product.GetAll();
+            return View(objProductList);
+        }
+
+        //UPSERT - GET
+        public IActionResult Upsert(int? id)
+        {
+            Product product = new();
+            if(id == null || id == 0)
+            {
+                //CREATE
+                return View(product);   
+            }
+            else
+            {
+                //UPDATE
+            }
+
+            return View(product);
+        }
+        //UPSERT - POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Product obj)
+        {
+            return View();
+        }
+    }
+}
