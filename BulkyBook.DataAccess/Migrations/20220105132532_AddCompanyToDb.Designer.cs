5e0555e99fdb4f3092f64dfd4e0788617d7d7b8f
stefcos24 <stefan22bl@gmail.com>
2022-01-11 13:39:39 +0100

    Added CompanyController,Model,View

--- BulkyBook.DataAccess/Migrations/20220105132532_AddCompanyToDb.Designer.cs
+++ BulkyBook.DataAccess/Migrations/20220105132532_AddCompanyToDb.Designer.cs
@@ -4,6 +4,7 @@ using BulkyBook.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BulkyBook.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220105132532_AddCompanyToDb")]
+    partial class AddCompanyToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -43,43 +45,6 @@ namespace BulkyBook.DataAccess.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("Categories");
-                });
-
-            modelBuilder.Entity("BulkyBook.Models.Company", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
-
-                    b.Property<string>("City")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("Name")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("PhoneNumber")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("PostalCode")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("State")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("StreetAddress")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.HasKey("Id");
-
-                    b.ToTable("Companies");
                 });
 
             modelBuilder.Entity("BulkyBook.Models.CoverType", b =>
---
+++ BulkyBook/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,96 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        //INDEX - GET
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        //UPSERT - GET
+        public IActionResult Upsert(int? id)
+        {
+            Company company = new();
+
+            if(id == null || id == 0)
+            {
+                //CREATE
+                return View(company);   
+            }
+            else
+            {
+                //UPDATE
+                company = _unitOfWork.Company.GetFirstOrDefault(u => u.Id == id);
+                return View(company);
+            }
+        }
+        //UPSERT - POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Company obj, IFormFile? file)
+        {
+            if (ModelState.IsValid)
+            {
+                if (obj.Id == 0)
+                {
+                    _unitOfWork.Company.Add(obj);
+                    _unitOfWork.Save();
+                    TempData["success"] = "Company created successfully";
+                }
+                else
+                {
+                    _unitOfWork.Company.Update(obj);
+                    _unitOfWork.Save();
+                    TempData["success"] = "Company updated successfully";
+                }
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var companyList = _unitOfWork.Company.GetAll();
+            return Json(new { data = companyList });
+        }
+
+        //POST
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var obj = _unitOfWork.Company.GetFirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            _unitOfWork.Company.Remove(obj);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Product deleted successful" });
+
+        }
+        #endregion
+    }
+
+}
