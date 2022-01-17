42ed1bd5ffdea1c1a97e08f2c8b22a07e260e60b
stefcos24 <stefan22bl@gmail.com>
2022-01-17 18:45:25 +0100

    Finished Orders and Summary

--- BulkyBook.Models/ViewModels/ShoppingCartVM.cs
+++ BulkyBook.Models/ViewModels/ShoppingCartVM.cs
@@ -9,6 +9,7 @@ namespace BulkyBook.Models.ViewModels
     public class ShoppingCartVM
     {
         public IEnumerable<ShoppingCart> ListCart { get; set; }
-        public double CartTotal { get; set; }
+        //public double CartTotal { get; set; } - This now we can use directly
+        public OrderHeader OrderHeader { get; set; }
     }
 }
